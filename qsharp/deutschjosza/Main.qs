dfa7e965f1ac58c6d19d8bbea6843d332bb98c15
Jorge <jorgematheusv@gmail.com>
2021-03-05 21:11:41 -0300

    Adding qiskit

--- qsharp/deutschjosza/Main.qs
+++ qsharp/deutschjosza/Main.qs
@@ -5,15 +5,17 @@ namespace DeutschJoszaNamespace {
     open Microsoft.Quantum.Canon;
     open Microsoft.Quantum.Intrinsic;
 
-    operation RunDeutschJosza(numQubits : Int) : Unit {
-        
-        let isBalanced = DeutschJosza(numQubits, OracleNQubit);
-        Message($"Algorithm is balanced: {isBalanced} ");
+    operation RunDeutschJosza(numQubits : Int, oracleId : Int) : Unit {
 
-        let isBalanced2 = DeutschJosza(numQubits, OracleAllZero);
-        Message($"Algorithm is balanced: {isBalanced2} ");
-
-        let isBalanced3 = DeutschJosza(numQubits, OracleAllOne);
-        Message($"Algorithm is balanced: {isBalanced2} ");
+        if(oracleId == 1){
+            let isBalanced = DeutschJosza(numQubits, OracleNQubit);
+            Message($"Algorithm is balanced: {isBalanced} ");
+        } elif (oracleId == 2) {
+            let isBalanced2 = DeutschJosza(numQubits, OracleAllZero);
+            Message($"Algorithm is balanced: {isBalanced2} ");
+        } elif (oracleId == 3) {
+            let isBalanced3 = DeutschJosza(numQubits, OracleAllOne);
+            Message($"Algorithm is balanced: {isBalanced3} ");
+        }
     }
 }
--- qsharp/deutschjosza/Wrapper.cs
+++ qsharp/deutschjosza/Wrapper.cs
@@ -4,7 +4,6 @@ using Microsoft.Quantum.Simulation.Simulators;
 using DeutschJoszaNamespace;
 using System.Diagnostics;
 
-
 namespace wrapper
 {
     static class Program
@@ -12,16 +11,20 @@ namespace wrapper
         static async Task Main(string[] args)
         {
 
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
             using var sim = new QuantumSimulator();
 
-            var deutschJoszaResult = await RunDeutschJosza.Run(sim, 32);
-            stopWatch.Stop();   
-            TimeSpan ts = stopWatch.Elapsed;
+            for (int numQubits = 3; numQubits < 32; numQubits++)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Restart(); 
+                var deutschJoszaResult = await RunDeutschJosza.Run(sim, numQubits, 1);
+                stopWatch.Stop();   
+                TimeSpan ts = stopWatch.Elapsed;
 
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                Console.WriteLine("Qubits: " + numQubits);
+                Console.WriteLine("RunTime: " + elapsedTime);
+            }
         }
     }
 }
